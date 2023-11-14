6d15ec4909917bfa928a6b7871a1d5cc435ca7b3
Mahendra <Mahendra@DESKTOP-VEO4E4O>
2023-11-14 10:42:41 +0530

    commit

--- src/stringlogical/noofOccurances.java
+++ src/stringlogical/noofOccurances.java
@@ -2,16 +2,19 @@ package stringlogical;
 
 public class noofOccurances {
 	public static void main(String[] args) {
-		String str="programming";
-		
-		char ch[]=str.toCharArray();
-		for(int i=0;i<ch.length;i++) {
-			int cnt=1;
-			for(int j=i+1;j<ch.length;j++) {
-				if(ch[i]==ch[j]) {
-					System.out.println(ch[i]+"="+cnt);
+		// String str="programming";
+		int arr[] = { 1, 2, 3, 2, 4, 1, 3, 4 };
+		// char ch[]=str.toCharArray();
+		// for(int i=0;i<ch.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
+			int cnt = 1;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
 					cnt++;
+					System.out.print(arr[i] + "=" + cnt + " ");
+
 				}
+
 			}
 		}
 	}
